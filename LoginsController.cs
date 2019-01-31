3ddac4ac9b14ab513af55ecffdc5b1cddbcd0e0e
chrisjoyce54 <chris@joycenet.us>
2019-01-30 18:23:19 -0600

    Add files via upload

---
+++ LoginsController.cs
@@ -0,0 +1,98 @@
+﻿using System.Threading.Tasks;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PersonalPhotos.Models;
+
+namespace PersonalPhotos.Controllers
+{
+    public class LoginsController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ILogins _loginService;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public LoginsController(ILogins loginService, IHttpContextAccessor httpContextAccessor, UserManager<IdentityUser> userManager)
+        {
+            _loginService = loginService;
+            _httpContextAccessor = httpContextAccessor;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index(string returnUrl = null)
+        {
+            var model = new LoginViewModel { ReturnUrl = returnUrl};
+            return View("Login", model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Invalid login detils");
+                return View("Login", model);
+            }
+
+            var user = await _loginService.GetUser(model.Email);
+            if (user != null)
+            {
+                if (user.Password == model.Password)
+                {
+                    //ToDo: redirect to home page
+                    _httpContextAccessor.HttpContext.Session.SetString("User", model.Email);
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid password");
+                    return View("Login", model);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "User was not found");
+                return View("Login", model);
+            }
+
+            if (!string.IsNullOrEmpty(model.ReturnUrl))
+            {
+                return Redirect(model.ReturnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Display", "Photos");
+            }
+        }
+
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Invalid user details");
+                return View(model);
+            }
+            var user  = new IdentityUser
+            {
+                UserName = model.Email,
+                Email = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, $"{error.Code}: {error.Description}");
+                }
+            }
+            return RedirectToAction("Index", "Logins");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,92 @@
+﻿using System.Reflection;
+using Core.Interfaces;
+using Core.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PersonalPhotos.Filters;
+
+namespace PersonalPhotos
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSession();
+            services.AddScoped<ILogins, SqlServerLogins>();
+            services.AddSingleton<IKeyGenerator, DefaultKeyGenerator>();
+            services.AddScoped<IPhotoMetaData, SqlPhotoMetaData>();
+            services.AddScoped<IFileStorage, LocalFileStorage>();
+            services.AddScoped<LoginAttribute>();
+
+            var connectionString = Configuration.GetConnectionString("Default");
+            var currentAssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+
+            services.AddDbContext<IdentityDbContext>(option =>
+            {
+                option.UseSqlServer(connectionString,
+                    obj => obj.MigrationsAssembly(currentAssemblyName));
+            });
+
+            services.AddIdentity<IdentityUser, IdentityRole>(option =>
+            {
+                new PasswordOptions
+                {
+                    RequireDigit = true,
+                    RequireLowercase = true,
+                    RequireUppercase = true,
+                    RequiredLength = 6
+                };
+                option.User = new UserOptions
+                {
+                    RequireUniqueEmail = true
+                };
+                option.SignIn = new SignInOptions
+                {
+                    RequireConfirmedEmail = false,
+                    RequireConfirmedPhoneNumber = false
+                };
+                option.Lockout = new LockoutOptions
+                {
+                    MaxFailedAccessAttempts = 3
+                };
+            }).AddEntityFrameworkStores<IdentityDbContext>().AddDefaultTokenProviders();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    "default",
+                    "{controller=Photos}/{action=Display}");
+            });
+        }
+    }
+}
